530ca4f717bd060cc6d07c4b231a0f0b6348f116
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2020-12-14 13:48:37 +0200

    Delete stringLength.js

--- JavaScript Advanced/JS Advanced/LAB SYNTAX, FUNCTIONS AND STATEMENTS/stringLength.js
+++
@@ -1,9 +0,0 @@
-function stringLength(first, second, third) {
-    let sumOfLength = first.length + second.length + third.length;
-    let averageScore = Math.floor(sumOfLength / 3);
-
-    console.log(sumOfLength);
-    console.log((averageScore));
-}
-
-stringLength('pasta', '5', '22.3');
