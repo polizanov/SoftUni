fc614406e276ebdb05c623d2d0d5074b4cf88b35
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-02-08 21:55:52 +0200

    Add files via upload

---
+++ JS Advanced/JS Advanced/LAB PROTOTYPES AND INHERITANCE/Person.js
@@ -0,0 +1,25 @@
+function Person(firstName, lastName){
+this.firstName = firstName;
+this.lastName = lastName;
+
+    Object.defineProperty(this, "fullName", {
+        enumerable: true,
+        get: function (){
+            return `${this.firstName} ${this.lastName}`
+        },
+
+        set: function(name){
+            [this.firstName, this.lastName] = name.split(" ");
+        }
+    })
+}
+
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName);//Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName);//George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName);//George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName)//Nikola
+console.log(person.lastName)//Tesla
