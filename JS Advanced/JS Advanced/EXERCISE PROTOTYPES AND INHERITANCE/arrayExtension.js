4e664037f610a86f31dc62ae4e1e6a5a0c2cb5d0
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-02-09 13:27:32 +0200

    Add files via upload

---
+++ JS Advanced/JS Advanced/EXERCISE PROTOTYPES AND INHERITANCE/arrayExtension.js
@@ -0,0 +1,7 @@
+(() => {
+    Array.prototype.last = function () { return this[this.length - 1] };
+    Array.prototype.skip = function (n) { return this.slice(n) };
+    Array.prototype.take = function (n) { return this.slice(0, n) };
+    Array.prototype.sum = function () { return this.reduce((acc, a) => acc + a, 0) };
+    Array.prototype.average = function () { return this.reduce((acc, a) => acc + a, 0) / this.length; };
+})()
