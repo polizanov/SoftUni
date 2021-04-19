0916d324fd5ad912591c576d96d1ecf5b88eead7
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-04-19 19:21:25 +0300

    added routes, templates and layouts

--- JS Web/JS Back-end/WORKSHOP EXPRESSJS AND TEMPLATING/config/express.js
+++ JS Web/JS Back-end/WORKSHOP EXPRESSJS AND TEMPLATING/config/express.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const handlebars = require("express-handlebars")
-const app = express();
 
 module.exports = (app) => {
     app.engine("hbs", handlebars({extname: ".hbs"}))
-    app.set("view-engine", "hbs")
+    app.set("view engine", "hbs");
+
+    app.use("/static", express.static("public"))
 }
--- JS Web/JS Back-end/WORKSHOP EXPRESSJS AND TEMPLATING/routes.js
+++ JS Web/JS Back-end/WORKSHOP EXPRESSJS AND TEMPLATING/routes.js
@@ -2,11 +2,24 @@ const { Router } = require("express");
 let router = Router();
 
 router.get("/", (req, res) => {
-    res.send('hone works');
+    res.render("home", {title: "Home page"})
 })
 
-// router.get("/about", (req, res) => {
+router.get("/about", (req, res) => {
+    res.render("about", {title: "About page"})
+})
 
-// })
+router.get("/create", (req, res) => {
+    res.render("create", {title: "Create page"})
+})
+
+router.post("/create", (req, res) => {
+    console.log(req.body)
+})
+
+router.get("/details/:id", (req, res) => {
+    res.render("details");
+    console.log(req.params.id);
+})
 
 module.exports = router;
