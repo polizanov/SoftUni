c9488c04bb041f84f787b76622de546da31f8a20
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-04-20 11:03:10 +0300

    added details functionality

--- JS Web/JS Back-end/WORKSHOP EXPRESSJS AND TEMPLATING/routes.js
+++ JS Web/JS Back-end/WORKSHOP EXPRESSJS AND TEMPLATING/routes.js
@@ -1,17 +1,18 @@
 const { Router } = require("express");
 const saveModel = require('./models/saveModel.js')
 let router = Router();
+let data = require("./models/db.json");
 
 router.get("/", (req, res) => {
-    res.render("home", {title: "Home page"})
+    res.render("home", { title: "Home page", data })
 })
 
 router.get("/about", (req, res) => {
-    res.render("about", {title: "About page"})
+    res.render("about", { title: "About page" })
 })
 
 router.get("/create", (req, res) => {
-    res.render("create", {title: "Create page"})
+    res.render("create", { title: "Create page" })
 })
 
 router.post("/create", saveModel, (req, res) => {
@@ -19,8 +20,15 @@ router.post("/create", saveModel, (req, res) => {
 })
 
 router.get("/details/:id", (req, res) => {
-    res.render("details");
-    console.log(req.params.id);
+    //console.log(req.params.id);
+    let detailsData = findCube(req.params.id);
+    console.log(detailsData)
+    res.render("details", { detailsData })
+
 })
+
+function findCube(id) {
+    return data.find(x => x.id == id);
+}
 
 module.exports = router;
